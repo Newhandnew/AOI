eb038ea164d567928979af5856597a420a33fa26
Newhandnew <easytired@gmail.com>
2018-03-19 13:57:32 +0800

    add scope for alexnet, add log_dir parameter for train and eval using flag

--- eval.py
+++ eval.py
@@ -1,64 +1,77 @@
 import tensorflow as tf
 import os
 import math
-import numpy as np
-from model import model
-from alexnet import alexnet_v2
+from alexnet import alexnet_v2, alexnet_my_arg_scope
 from read_tfrecord import get_data_batch, get_record_number
+
 slim = tf.contrib.slim
 
-data_dir = 'data'
-tfrecord_test = 'AOI_test.tfrecords'
-test_tf_path = os.path.join(data_dir, tfrecord_test)
-logs_path = "logs_alex"
-crop_size = [224, 224]
-num_classes = 2
+flags = tf.app.flags
+flags.DEFINE_string('logs_dir', '',
+                    'Directory to save the checkpoints and training summaries.')
+FLAGS = flags.FLAGS
 
-num_examples = get_record_number(test_tf_path)
-batch_size = 32
-num_batches = math.ceil(num_examples / float(batch_size))
-# Load the data
-test_image_batch, test_label_batch = get_data_batch(
-    test_tf_path, crop_size, batch_size, is_training=False, one_hot=False)
-# convert to float batch
-test_image_batch = tf.to_float(test_image_batch)
-# Define the network
-logits, _ = alexnet_v2(test_image_batch, num_classes=num_classes, is_training=False)
 
-predictions = tf.argmax(logits, 1)
+def main(_):
+    """
+    Configuration Part.
+    """
+    assert FLAGS.logs_dir, '`logs_dir` is missing.'
+    logs_path = os.path.join('logs', FLAGS.logs_dir)
+    data_dir = 'data'
+    tfrecord_test = 'AOI_test.tfrecords'
+    test_tf_path = os.path.join(data_dir, tfrecord_test)
+    crop_size = [224, 224]
+    num_classes = 2
 
-# Choose the metrics to compute:
-# names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
-#     'accuracy': slim.metrics.accuracy(predictions, test_label_batch),
-# })
-names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
-    'test/Accuracy': slim.metrics.streaming_accuracy(predictions, test_label_batch),
-    'test/mse': slim.metrics.streaming_mean_squared_error(predictions, test_label_batch),
-    # 'test/Recall@5': slim.metrics.streaming_recall_at_k(logits, test_label, 5),
-})
-for name, tensor in names_to_updates.items():
-    tf.summary.scalar(name, tensor)
-# Create the summary ops such that they also print out to std output:
-summary_ops = []
-for metric_name, metric_value in names_to_values.items():
-  op = tf.summary.scalar(metric_name, metric_value)
-  op = tf.Print(op, [metric_value], metric_name)
-  summary_ops.append(op)
+    num_examples = get_record_number(test_tf_path)
+    batch_size = 32
+    num_batches = math.ceil(num_examples / float(batch_size))
+    # Load the data
+    test_image_batch, test_label_batch = get_data_batch(
+        test_tf_path, crop_size, batch_size, is_training=False, one_hot=False)
+    # convert to float batch
+    test_image_batch = tf.to_float(test_image_batch)
+    # Define the network
+    with slim.arg_scope(alexnet_my_arg_scope(is_training=False)):
+        logits, _ = alexnet_v2(test_image_batch, num_classes=num_classes, is_training=False)
 
-# Setup the global step.
-slim.get_or_create_global_step()
-session_config = tf.ConfigProto()
-session_config.gpu_options.allow_growth = True
-session = tf.Session(config=session_config)
-tf.logging.set_verbosity(tf.logging.INFO)
-output_dir = logs_path # Where the summaries are stored.
-eval_interval_secs = 10 # How often to run the evaluation.
-slim.evaluation.evaluation_loop(
-    '',
-    logs_path,
-    logs_path,
-    num_evals=num_batches,
-    eval_op=list(names_to_updates.values()),
-    summary_op=tf.summary.merge(summary_ops),
-    eval_interval_secs=eval_interval_secs)
+    predictions = tf.argmax(logits, 1)
 
+    # Choose the metrics to compute:
+    # names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
+    #     'accuracy': slim.metrics.accuracy(predictions, test_label_batch),
+    # })
+    names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
+        'test/Accuracy': slim.metrics.streaming_accuracy(predictions, test_label_batch),
+        'test/mse': slim.metrics.streaming_mean_squared_error(predictions, test_label_batch),
+        # 'test/Recall@5': slim.metrics.streaming_recall_at_k(logits, test_label, 5),
+    })
+    for name, tensor in names_to_updates.items():
+        tf.summary.scalar(name, tensor)
+    # Create the summary ops such that they also print out to std output:
+    summary_ops = []
+    for metric_name, metric_value in names_to_values.items():
+      op = tf.summary.scalar(metric_name, metric_value)
+      op = tf.Print(op, [metric_value], metric_name)
+      summary_ops.append(op)
+
+    # Setup the global step.
+    slim.get_or_create_global_step()
+    session_config = tf.ConfigProto()
+    session_config.gpu_options.allow_growth = True
+    session = tf.Session(config=session_config)
+    tf.logging.set_verbosity(tf.logging.INFO)
+    eval_interval_secs = 10 # How often to run the evaluation.
+    slim.evaluation.evaluation_loop(
+        '',
+        logs_path,
+        logs_path,
+        num_evals=num_batches,
+        eval_op=list(names_to_updates.values()),
+        summary_op=tf.summary.merge(summary_ops),
+        eval_interval_secs=eval_interval_secs)
+
+
+if __name__ == '__main__':
+    tf.app.run()
--- train.py
+++ train.py
@@ -1,102 +1,109 @@
 import os
 import tensorflow as tf
 import math
-from alexnet import alexnet_v2
-from model import model
+from alexnet import alexnet_v2, alexnet_v2_arg_scope, alexnet_my_arg_scope
 from read_tfrecord import get_data_batch, get_record_number
 
 slim = tf.contrib.slim
 
-"""
-Configuration Part.
-"""
-data_dir = 'data'
-tfrecord_train = 'AOI_train.tfrecords'
-load_checkpoint = True
-train_tf_path = os.path.join(data_dir, tfrecord_train)
+flags = tf.app.flags
+flags.DEFINE_string('logs_dir', '',
+                    'Directory to save the checkpoints and training summaries.')
+FLAGS = flags.FLAGS
 
-crop_size = [224, 224]
-# Learning params
-learning_rate = 0.01
-num_epochs = 500
-batch_size = 64
-num_examples = get_record_number(train_tf_path)
-num_batches = math.ceil(num_examples / float(batch_size))
-print('batch number: {}'.format(num_batches))
 
-# Network params
-dropout_rate = 0.5
-num_classes = 2
-channel = 1
+def main(_):
+    """
+    Configuration Part.
+    """
+    assert FLAGS.logs_dir, '`logs_dir` is missing.'
+    logs_path = os.path.join('logs', FLAGS.logs_dir)
+    data_dir = 'data'
+    tfrecord_train = 'AOI_train.tfrecords'
+    load_checkpoint = True
+    train_tf_path = os.path.join(data_dir, tfrecord_train)
 
-checkpoint_dir = "checkpoint_alex"
-checkpoint_name = 'model.ckpt'
-logs_path = "logs_alex"
+    crop_size = [224, 224]
+    # Learning params
+    learning_rate = 0.01
+    num_epochs = 500
+    batch_size = 64
+    num_examples = get_record_number(train_tf_path)
+    num_batches = math.ceil(num_examples / float(batch_size))
+    print('batch number: {}'.format(num_batches))
 
-# Launch the graph
-with tf.Graph().as_default():
+    # Network params
+    num_classes = 2
 
-    tf.logging.set_verbosity(tf.logging.INFO)
-    tf.summary.scalar('batch_size', batch_size)
+    # Launch the graph
+    with tf.Graph().as_default():
 
-    # Load the data
-    train_image_batch, train_label_batch = get_data_batch(
-        train_tf_path, crop_size, batch_size, is_training=True, one_hot=False)
-    # convert to float batch
-    train_image_batch = tf.to_float(train_image_batch)
+        tf.logging.set_verbosity(tf.logging.INFO)
+        tf.summary.scalar('batch_size', batch_size)
 
-    tf.summary.image('image', train_image_batch)
+        # Load the data
+        train_image_batch, train_label_batch = get_data_batch(
+            train_tf_path, crop_size, batch_size, is_training=True, one_hot=False)
+        # convert to float batch
+        train_image_batch = tf.to_float(train_image_batch)
 
-    net, _ = alexnet_v2(train_image_batch, num_classes=num_classes, is_training=True)
-    # make summaries of every operation in the node
-    # for layer_name, layer_op in end_points.items():
-    #     tf.summary.histogram(layer_name, layer_op)
+        tf.summary.image('image', train_image_batch)
 
-    # Specify the loss function (outside the model!)
-    # one_hot_labels = slim.one_hot_encoding(train_label_batch, num_classes)
-    one_hot_label = tf.one_hot(indices=train_label_batch, depth=num_classes)
-    slim.losses.softmax_cross_entropy(net, one_hot_label)
-    total_loss = slim.losses.get_total_loss()
+        with slim.arg_scope(alexnet_my_arg_scope(is_training=True)):
+            net, _ = alexnet_v2(train_image_batch, num_classes=num_classes, is_training=True)
+        # make summaries of every operation in the node
+        # for layer_name, layer_op in end_points.items():
+        #     tf.summary.histogram(layer_name, layer_op)
 
-    # Create some summaries to visualize the training process:
-    tf.summary.scalar('losses/Total Loss', total_loss)
+        # Specify the loss function (outside the model!)
+        # one_hot_labels = slim.one_hot_encoding(train_label_batch, num_classes)
+        one_hot_label = tf.one_hot(indices=train_label_batch, depth=num_classes)
+        slim.losses.softmax_cross_entropy(net, one_hot_label)
+        total_loss = slim.losses.get_total_loss()
 
-    # Specify the optimizer and create the train op:
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    train_op = slim.learning.create_train_op(total_loss, optimizer)
+        # Create some summaries to visualize the training process:
+        tf.summary.scalar('losses/Total Loss', total_loss)
 
-    # Track accuracy and recall
-    predictions = tf.argmax(net, 1)
+        # Specify the optimizer and create the train op:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+        train_op = slim.learning.create_train_op(total_loss, optimizer)
 
-    # Define the metrics:
-    # Recall@5 would make no sense, because we have only 5 classes here
-    names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
-        'eval/Accuracy': slim.metrics.streaming_accuracy(predictions, train_label_batch),
-        'eval/Recall@2': slim.metrics.streaming_recall_at_k(net, train_label_batch, 2),
-    })
-    for name, tensor in names_to_updates.items():
-        tf.summary.scalar(name, tensor)
-    saver = tf.train.Saver()
-    session_config = tf.ConfigProto()
-    session_config.gpu_options.allow_growth = True
-    session_config.log_device_placement = True
-    session = tf.Session(config=session_config)
-    prev_model = tf.train.get_checkpoint_state(logs_path)
-    if load_checkpoint:
-        if prev_model:
-            saver.restore(session, prev_model.model_checkpoint_path)
-            print('Checkpoint found, {}'.format(prev_model))
-        else:
-            print('No checkpoint found')
-# Run the training:
-    final_loss = slim.learning.train(
-        train_op,
-        logdir=logs_path,
-        number_of_steps=num_epochs * num_batches,
-        session_config=session_config,
-        save_summaries_secs=20,
-        save_interval_secs=300
-    )
+        # Track accuracy and recall
+        predictions = tf.argmax(net, 1)
 
-    print('Finished training. Final batch loss %d' % final_loss)
+        # Define the metrics:
+        # Recall@5 would make no sense, because we have only 5 classes here
+        names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
+            'eval/Accuracy': slim.metrics.streaming_accuracy(predictions, train_label_batch),
+            'eval/Recall@2': slim.metrics.streaming_recall_at_k(net, train_label_batch, 2),
+        })
+        for name, tensor in names_to_updates.items():
+            tf.summary.scalar(name, tensor)
+        saver = tf.train.Saver()
+        session_config = tf.ConfigProto()
+        session_config.gpu_options.allow_growth = True
+        session_config.log_device_placement = True
+        session = tf.Session(config=session_config)
+        prev_model = tf.train.get_checkpoint_state(logs_path)
+        if load_checkpoint:
+            if prev_model:
+                saver.restore(session, prev_model.model_checkpoint_path)
+                print('Checkpoint found, {}'.format(prev_model))
+            else:
+                print('No checkpoint found')
+    # Run the training:
+        final_loss = slim.learning.train(
+            train_op,
+            logdir=logs_path,
+            number_of_steps=num_epochs * num_batches,
+            session_config=session_config,
+            save_summaries_secs=20,
+            save_interval_secs=300
+        )
+
+        print('Finished training. Final batch loss %d' % final_loss)
+
+
+if __name__ == '__main__':
+    tf.app.run()
 
